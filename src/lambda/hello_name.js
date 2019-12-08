84e96eafaf9c096bf4689eb6680b082ddf8b51b0
XPeppers <nicola.margoni@xpeppers.com>
2019-12-08 20:22:37 +0100

    updates hello_name function

--- src/lambda/hello_name.js
+++ src/lambda/hello_name.js
@@ -1,8 +1,13 @@
 exports.handler = async (event, context) => {
   const name = event.queryStringParameters.name || "World";
 
+  let payload = {
+  	temperature: Math.round(Math.random() * (30 - (-10)) + (-10))
+  }
+
   return {
     statusCode: 200,
-    body: `Hello, ${name}`
+    body: JSON.stringify(payload)
   };
+
 };
