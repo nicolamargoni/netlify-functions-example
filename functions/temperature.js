04f18db1bfd5d21ae612afef3773e8d5587184cf
XPeppers <nicola.margoni@xpeppers.com>
2019-12-08 21:03:45 +0100

    adds fake temp

--- functions/temperature.js
+++ functions/temperature.js
@@ -1,5 +1,16 @@
 exports.handler = async (event, context) => {
-  const name = event.queryStringParameters.name || "World";
+  const fakeTemp = event.queryStringParameters.fakeTemp;
+
+  if(fakeTemp) {
+  	let payload = {
+  		temperature: fakeTemp
+  	}
+  	 
+  	return {
+    	statusCode: 200,
+    	body: JSON.stringify(payload)
+    }
+  }
 
   let payload = {
   	temperature: Math.round(Math.random() * (30 - (-10)) + (-10))
